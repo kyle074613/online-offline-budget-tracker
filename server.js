6ad4eafe41d5110424f874843467cec0668a682a
kyle074613 <kylemj@uci.edu>
2020-03-29 21:54:04 -0700

    trying to fix heroku bugs

--- server.js
+++ server.js
@@ -16,15 +16,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
-
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 // routes
-require("./routes/htmlRoutes.js")(app);
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
